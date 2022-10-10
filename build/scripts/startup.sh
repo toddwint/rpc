bd9ae8252654fa5c51e43e5062bc91e2407cd0d8
toddwint <todd.wintermute@gmail.com>
2022-10-10 16:40:49 -0500

    first commit

---
+++ build/scripts/startup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+## Run the commands to make it all work
+ln -fs /usr/share/zoneinfo/$TZ /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+echo $HOSTNAME > /etc/hostname
+
+frontail -d -p $HTTPPORT /var/log/syslog
+
+# Keep docker running
+bash
